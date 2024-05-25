0c129e581a1ad942f9e1e59bee19fb0d221193bc
sonalbha <bhayanis151@gmail.com>
2024-05-25 15:01:31 +0530

    Add files via upload

---
+++ v4.php
@@ -0,0 +1,16 @@
+<?php 
+    // echo "hello world";
+    // echo 'hello world1';
+    // echo "hello ","world2";
+    // echo "hello "."world3";
+    // echo 25;
+    // echo "<h1>Hello Sandy</h1>";
+
+
+    // print "hello world4";
+    // print 'hello world5';
+    // print "hello "."world6";
+    // print 25.52;
+    print "<h2>Hello sandy2</h2>";
+
+?>
