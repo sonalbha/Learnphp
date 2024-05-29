4876d49ad1aa8c9273e2914a2d0bbc3103cead6d
sonalbha <bhayanis151@gmail.com>
2024-05-29 20:51:46 +0530

    Add files via upload

---
+++ v7.php
@@ -0,0 +1,12 @@
+<?php 
+    $x="hello";
+    $x=25;
+    $x=25.5;
+    $x=false;
+    $x=array("html","css","javascript");
+    $x=null;
+    echo $x."</br>";
+    // print_r($x);
+    var_dump($x);
+
+?>
