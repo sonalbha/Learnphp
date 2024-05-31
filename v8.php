41fd8308494e6d061de2307a88f948935f8de646
sonalbha <bhayanis151@gmail.com>
2024-05-31 22:56:58 +0530

    Add files via upload

---
+++ v8.php
@@ -0,0 +1,12 @@
+<?php  
+    $a="hello Sandy"; //this is message
+    echo $a; # print message
+
+    $b=10;
+    // echo $b;
+
+
+    /* this is a multiple line comments
+        here print message above */
+        echo $b;
+?>
