4ae5ec1de3c0765be4daf9e396ef8b2d24d72b49
sonalbha <bhayanis151@gmail.com>
2024-06-06 09:05:31 +0530

    Add files via upload

---
+++ v10.php
@@ -0,0 +1,22 @@
+<?php 
+    $a=10;
+    $b=3;
+    $add=$a+$b;
+    $sub=$a-$b;
+    $mul=$a*$b;
+    $div=$a/$b;
+    $pow=$a**$b;
+    // $a++;
+    // ++$a;
+    // $a--;
+    // --$a;
+    $mod=$a%$b;
+    echo "add : ".$add."</br>";
+    echo "sub : ".$sub."</br>";
+    echo "mul : ".$mul."</br>";
+    echo "div : ".$div."</br>";
+    echo "power : ".$pow."</br>";
+    echo "mod : ".$mod."</br>";
+    // echo "increment : ".$a."</br>";
+    // echo "decrement : ".$a."</br>";
+?>
